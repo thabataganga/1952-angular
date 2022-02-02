74a9b8edfeed053ed565cc94bb0e434c2b6c43a1
thabataganga <ganga.thabata@gmail.com>
2022-02-02 00:53:38 -0300

    get da api

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,5 +1,6 @@
-import { TransferenciaService } from './../services/transferencia.service';
+import { Transferencia } from './../models/transferencia.models';
 import { Component, Input, OnInit } from '@angular/core';
+import { TransferenciaService } from './../services/transferencia.service';
 
 @Component({
   selector: 'app-extrato',
@@ -12,6 +13,9 @@ export class ExtratoComponent implements OnInit {
   constructor(private service: TransferenciaService) {}
 
   ngOnInit(): void {
-    this.transferencias = this.service.transferencias;
+    this.service.todas().subscribe((transferencias: Transferencia[]) => {
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    });
   }
 }
